71f566599a7096a88d4e2bde0ddf766ec6f8501c
sangeeta <sangeetabhatia03@gmail.com>
2023-08-21 16:50:16 +0100

    renv infrastructure

--- src/sim_baseline_estim/orderly.R
+++ src/sim_baseline_estim/orderly.R
@@ -6,6 +6,10 @@ orderly_dependency(
   "sim_data_baseline", "latest",
   c(sim_data_baseline.rds = "sim_data_baseline.rds")
 )
+orderly_artefact(
+  "MCMC output for baseline sim",
+  "sim_baseline_estim.rds"
+)
 library(MixDiff)
 sim_params <- readRDS("sim_params.rds")
 index_dates <- sim_params$index_dates
@@ -26,14 +30,15 @@ mcmc_settings <- list(
     ),
     sdlog_CV = list(0.25, c(0.25, 0.25), c(0.25, 0.25, 0.25), c(0.25, 0.25, 0.25))),
   init_options = list(mindelay=0, maxdelay=100),
-  chain_properties=list(n_iter = 5000, burnin = 500, record_every=10)
+  chain_properties=list(n_iter = 500, burnin = 50, record_every = 10)
 )
 
 hyperparameters <- list(
   shape1_prob_error=3,
   shape2_prob_error=12,
   mean_mean_delay=100,
-  mean_CV_delay=100)
+  mean_CV_delay=100
+)
 
 mcmc_out <- RunMCMC(
   sim_data_baseline,
@@ -41,3 +46,5 @@ mcmc_out <- RunMCMC(
   hyperparameters,
   index_dates
 )
+
+saveRDS(mcmc_out, "sim_baseline_estim.rds")
