18d154f17cbc1cd5420a0538ac1f5f6b0fc620cc
rebeccanash <r.nash@imperial.ac.uk>
2025-05-22 18:24:39 +0100

    Start workflow script

---
+++ workflow.R
@@ -0,0 +1,10 @@
+library(orderly2)
+
+# Simulation parameters
+orderly_run("sim_params")
+
+# Simulate baseline data
+orderly_run("sim_data_baseline", parameters = list(nsims = 1))
+
+# MCMC output for baseline estim
+orderly_run("sim_baseline_estim")
