dc4af6c3171c195515dea2286214b77490876bd5
rebeccanash <r.nash@imperial.ac.uk>
2025-06-10 10:19:09 +0100

    Update workflow script

--- workflow.R
+++ workflow.R
@@ -1,10 +1,35 @@
 library(orderly2)
 
-# Simulation parameters
+# Create a named list containing the simulation parameters for all scenarios
 orderly_run("sim_params")
 
-# Simulate baseline data
-orderly_run("sim_data_baseline", parameters = list(nsims = 1))
+# Simulate data for all scenarios
+orderly_run("sim_data", parameters = list(nsims = 100))
 
-# MCMC output for baseline estim
-orderly_run("sim_estim_baseline")
+# MCMC output -----------------------------------------------------------------
+orderly_run("sim_estim", parameters = list(scenario = c("baseline"))) # done
+
+orderly_run("sim_estim", parameters = list(scenario = c("low_missingness"))) # done
+
+orderly_run("sim_estim", parameters = list(scenario = c("very_small_sample"))) # ISSUE - just stops for no reason
+orderly_run("sim_estim", parameters = list(scenario = c("small_sample")))
+orderly_run("sim_estim", parameters = list(scenario = c("moderate_sample"))) # done
+orderly_run("sim_estim", parameters = list(scenario = c("very_large_sample"))) # done - ran very slowly (10 hours!)
+
+orderly_run("sim_estim", parameters = list(scenario = c("low_error"))) # done
+orderly_run("sim_estim", parameters = list(scenario = c("high_error"))) # done
+
+orderly_run("sim_estim", parameters = list(scenario = c("long_delays"))) # done
+orderly_run("sim_estim", parameters = list(scenario = c("short_delays")))
+
+
+# running in scenario batches
+scenarios <- c("very_small_sample",
+               "small_sample",
+               "moderate_sample",
+               "very_large_sample")
+
+for (s in scenarios) {
+  orderly_run("sim_estim", parameters = list(scenario = s))
+}
+
