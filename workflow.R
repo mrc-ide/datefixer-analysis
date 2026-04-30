2b315972d296d92f10411cea4c29c70b73458df4
rebeccanash <r.nash@imperial.ac.uk>
2026-04-30 15:43:54 +0100

    latest result IDs

--- workflow.R
+++ workflow.R
@@ -285,7 +285,7 @@ sanity <- task_create_expr(
 
 task_info(sanity)
 #sanity <- "072a39f93b4f1dd4831ec713feb93105"
-task_result(sanity) # 
+task_result(sanity) # "20260430-131813-075b3c00"
 
 ## variable error diagnostics -----------------------
 
@@ -300,7 +300,7 @@ variable_error <- task_create_expr(
 
 task_info(variable_error)
 #variable_error <- "6b4a7e31a58c21f91433ef2432c2b568"
-task_result(variable_error) # 
+task_result(variable_error) # "20260430-131826-24b5b841"
 
 ## variable group sample size -----------------------
 
@@ -315,7 +315,7 @@ variable_sample <- task_create_expr(
 
 task_info(variable_sample)
 #variable_sample <- "5e0c77f00778dc60d5a322d89398a3c4"
-task_result(variable_sample) # 
+task_result(variable_sample) # "20260430-131839-ed55eebe"
 
 
 ## variable delay diagnostics -----------------------
@@ -331,7 +331,7 @@ variable_delays <- task_create_expr(
 
 task_info(variable_delays)
 #variable_delays <- "2d748dc746b6ea932c13310be92c00bc"
-task_result(variable_delays) # 
+task_result(variable_delays) # "20260430-131850-d3c23c88"
 
 ## variable cv -----------------------
 
@@ -346,7 +346,7 @@ variable_cv <- task_create_expr(
 
 task_info(variable_cv)
 #variable_cv <- "c58b573d51a68d7008f7d3774234a711"
-task_result(variable_cv) # 
+task_result(variable_cv) # "20260430-131854-66372454"
 
 
 ## variable delay type -----------------------
@@ -362,4 +362,4 @@ variable_distr <- task_create_expr(
 
 task_info(variable_distr)
 #variable_distr <- "61c5825a964fa4d3fd28b59fad6f672d"
-task_result(variable_distr) # 
+task_result(variable_distr) # "20260430-131904-d42eeb20"
